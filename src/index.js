13b3c264f6fec9c05d0a2716dfc6a9fb118a9b3d
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-07-27 00:18:42 +0530

    added analytics

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import Hero from "./Hero";
 import Playlist from "./Playlist";
+import {inject} from "@vercel/analytics";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Hero />
     <App />
     <Playlist />
-  </React.StrictMode>
-);
 
+  </React.StrictMode>
+
+);
+inject();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
